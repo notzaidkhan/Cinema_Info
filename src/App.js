293bb034d47acc5f276293416df9855db26795d2
notzaidkhan <theknight7772@gmail.com>
2024-06-05 20:34:37 +0530

    add route to Imdb Pages

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import MovieCard from "./MovieCard";
 import SearchIcon from "./search.svg";
 import "./App.css";
@@ -17,43 +17,51 @@ const App = () => {
   const searchMovies = async (title) => {
     const response = await fetch(`${API_URL}&s=${title}`);
     const data = await response.json();
-
+    console.log(data);
     setMovies(data.Search);
   };
-
   return (
-    <div className="app">
+    <div>
+      <Router>
+        <Routes>
+          <Route path="/" />
+          <Route path="/other" />
+        </Routes>
+      </Router>
+      <div className="app">
 
-      <h1>MovieInfo</h1>
+        <h1 style={{color: "#cc090c"}}>MovieInfo</h1>
 
-      <div className="search">
-        
-        <input
-          value={searchTerm}
-          onChange={(e) => setSearchTerm(e.target.value)}
-          placeholder="Search for movies"
-        />
+        <div className="search">
 
-        <img
-          src={SearchIcon}
-          alt="search"
-          onClick={() => searchMovies(searchTerm)}
-        />
+          <input
+            value={searchTerm}
+            onChange={(e) => setSearchTerm(e.target.value)}
+            placeholder="Search for movies"
+          />
 
+          <img
+            src={SearchIcon}
+            alt="search"
+            onClick={() => searchMovies(searchTerm)}
+          />
+
+        </div>
+
+        {movies?.length > 0 ? (
+          <div className="container">
+            {movies.map((movie) => (
+              <MovieCard movie={movie} />
+            ))}
+          </div>
+        ) : (
+          <div className="empty">
+            <h2>No movies found</h2>
+          </div>
+        )}
       </div>
-    
-      {movies?.length > 0 ? (
-        <div className="container">
-          {movies.map((movie) => (
-            <MovieCard movie={movie} />
-          ))}
-        </div>
-      ) : (
-        <div className="empty">
-          <h2>No movies found</h2>
-        </div>
-      )}
     </div>
+
   );
 };
 
--- src/MovieCard.jsx
+++ src/MovieCard.jsx
@@ -1,8 +1,14 @@
 import React from 'react';
+// import { useNavigate } from 'react-router-dom';
 
 const MovieCard = ({ movie: { imdbID, Year, Poster, Title, Type } }) => {
+
+  const handleClick = () => {
+    window.location.href = `https://www.imdb.com/title/${imdbID}`;
+  };
+  
   return (
-    <div className="movie" key={imdbID}>
+    <div className="movie" key={imdbID} onClick={handleClick} style={{ cursor: 'pointer' }}>
       <div>
         <p>{Year}</p>
       </div>
